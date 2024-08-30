0ee79f026d99712e8c3e3bad3d5f4768f5b78969
Juan Ramos <juanl.bnr@gmail.com>
2024-08-30 18:42:38 -0300

    erro de cors
    Resolvido erro de cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
 
 const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
+app.use(cors(/*aqui coloca a pagina que pode acessar o backend*/))
 
 app.post('/usuarios', async (req, res) => {
     await prisma.user.create({
